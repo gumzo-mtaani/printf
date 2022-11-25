cef19156df8759edfb18df12e41d6593e9984cb4
Paul Njuga <paulnjuga@gmail.com>
2022-11-25 04:26:00 +0300

    Add unsigned int test cases

--- test/main.c
+++ test/main.c
@@ -10,9 +10,11 @@
 int main(void)
 {
 	int len, len2;
+	unsigned int ui;
 
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
+	ui = (unsigned int)INT_MAX + 1024;
 
 	_printf("Character:[%c]\n", 'H');
 	printf("Character:[%c]\n", 'H');
@@ -24,6 +26,9 @@ int main(void)
 	printf("Length:[%d, %i]\n", len2, len2);
 	_printf("Negative:[%d]\n", -762534);
 	printf("Negative:[%d]\n", -762534);
+
+	_printf("Unsigned:[%u]\n", ui);
+	printf("Unsigned:[%u]\n", ui);
 
 	len = _printf("Percent:[%%]\n");
 	len2 = printf("Percent:[%%]\n");
