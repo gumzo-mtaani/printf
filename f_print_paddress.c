3ca88fc76315904f4c2c62b13d4b11b98466c02c
Paul Njuga <paulnjuga@gmail.com>
2022-11-27 20:50:19 +0300

    Create %p cs handler

---
+++ f_print_paddress.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_paddress - prints address of input in HEX format
+ * @list: va_list arguments from _printf
+ *
+ * Return: number of char printed
+ */
+int print_paddress(va_list list)
+{
+	char *str;
+	unsigned long int p = va_arg(list, unsigned long int);
+	register int count = 0; /* Return value */
+
+	if (!p)
+		return (_puts("(nil)")); /* Null pointer */
+
+	str = conv(p, 16, 0);
+	count += _puts("0x"); /* 0x(HEX value) */
+	count += _puts(str);
+
+}
